4cc651647670c472ab6c28af7256b359642d3b28
abhikur <thakura204@gmail.com>
2016-02-17 09:39:02 +0530

    Class for basic operation is made

---
+++ MathsOperations.java
@@ -0,0 +1,8 @@
+class MathsOperations {
+	public static void main(String[] args) {
+		BasicOperations operation = new BasicOperations();
+		System.out.println(operation.areaOfCircle(7));
+		System.out.println(operation.lcm(3,8));
+		System.out.println(operation.hcf(3,8));
+	}
+}
